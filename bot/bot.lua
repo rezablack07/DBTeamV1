3310d96217b3f2e27112ddcfa29fb3b325179a22
rezablack077 <oxford_sajadi@hotmail.com>
2017-01-16 18:16:15 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+    sudo_users = {256331224,193068389},
     admin_users = {},
     disabled_channels = {}
   }
